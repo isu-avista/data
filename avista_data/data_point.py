4175547b1f337309b144fbcf864098cbe9d26648
grifisaa <grifisaa@isu.edu>
2020-11-25 01:57:39 -0700

    updated data point to include timestamp, fixed unit tests, and added migration

--- avista_data/data_point.py
+++ avista_data/data_point.py
@@ -7,6 +7,7 @@ class DataPoint(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.Integer, nullable=False)
     unit = db.Column(db.Enum(Unit), nullable=False)
     sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.id'))
 
@@ -17,6 +18,22 @@ class DataPoint(db.Model):
         if not isinstance(value, float):
             raise Exception("value is not a float")
         self.value = value
+
+    def get_timestamp(self):
+        return self.timestamp
+
+    def set_timestamp(self, timestamp):
+        if not isinstance(timestamp, int):
+            raise Exception("timestamp is not an int")
+        self.timestamp = timestamp
+
+    def get_unit(self):
+        return self.unit
+
+    def set_unit(self, unit):
+        if unit is None:
+            raise Exception("Unit cannot be none")
+        self.unit = unit
 
     def __repr__(self):
         return "Data Point: {}".format(self.value)
