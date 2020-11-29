1d55f9d7876217dc69a25f77917b22c165215851
grifisaa <grifisaa@isu.edu>
2020-11-28 18:52:10 -0700

    updated

--- avista_data/user.py
+++ avista_data/user.py
@@ -20,7 +20,6 @@ class User(db.Model):
     last_name = db.Column(db.String(75), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-    token = db.Column(db.String(32), unique=True)
     role = db.Column(db.Enum(Role), nullable=False)
     sec_conf_id = db.Column(db.Integer, db.ForeignKey('security_config.id'))
     api_keys = db.relationship('ApiKey', backref='user', lazy='dynamic')
